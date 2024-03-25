ffaac17b557a0d6ff37de39cd16b2fa5cde11e2f
rjunio98 <robledorjunior1@gmail.com>
2024-03-24 22:08:54 -0300

    Adiciona validação de endereço e verificação de horário de funcionamento

--- script.js
+++ script.js
@@ -117,3 +117,67 @@ function removeItemCart(name) {
     updateCartModal();
   }
 }
+
+addressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
+
+checkoutBtn.addEventListener("click", function () {
+  /* 
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen) {
+        alert("O restaurante está fechado");
+        return;
+    }
+  */
+
+  if (cart.length === 0) return;
+
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+    return;
+  }
+
+  const cartItems = cart
+    .map((item) => {
+      return `
+      ${item.name} Quantidade: (${
+        item.quantity
+      }) Preço: R$ ${item.price.toFixed(2)}
+      |
+    `;
+    })
+    .join("");
+
+  const message = encodeURIComponent(cartItems);
+  const phone = "71993614955";
+
+  window.open(
+    `https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`
+  ),
+    "_blank";
+});
+
+function checkRestaurantOpen() {
+  const data = new Date();
+  const hora = data.getHours();
+
+  return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else {
+  spanItem.classList.remove("bg-green-600");
+  spanItem.classList.add("bg-red-500");
+}
